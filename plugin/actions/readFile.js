32635546f294cbc6909aedfdcc83654d64abaf92
zdying <zdying@live.com>
2017-10-11 12:04:21 +0800

    support read custom rewrite snippet

--- plugin/actions/readFile.js
+++ plugin/actions/readFile.js
@@ -7,17 +7,31 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (data, req, res) {
-  var file = path.resolve(data.file);
+  var originFile = data.file;
+  var file = path.resolve(originFile);
+
   res.writeHead(200, {
     'Content-Type': 'application/json'
   });
   fs.readFile(file, 'utf-8', function (err, string) {
     if (err) {
-      res.end(JSON.stringify({
-        status: 1,
-        message: err.message,
-        data: {}
-      }));
+      var rewriteFiles = global.hiproxyServer.rewrite._files;
+      if (rewriteFiles[originFile] && rewriteFiles[originFile].source) {
+        res.end(JSON.stringify({
+          status: 0,
+          message: '',
+          data: {
+            content: rewriteFiles[originFile].source,
+            file: originFile
+          }
+        }));
+      } else {
+        res.end(JSON.stringify({
+          status: 1,
+          message: err.message,
+          data: {}
+        }));
+      }
     } else {
       res.end(JSON.stringify({
         status: 0,
