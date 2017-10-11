1e91316d0c4443cb8cb3eaf438514e2ade60093e
zdying <zdying@live.com>
2017-10-11 15:00:25 +0800

    support edit custom snippet

--- plugin/actions/saveFile.js
+++ plugin/actions/saveFile.js
@@ -7,6 +7,19 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (data, req, res) {
+  var rewriteFiles = global.hiproxyServer.rewrite._files;
+  var fileInfo = rewriteFiles[data.file];
+
+  if (fileInfo && fileInfo.source) {
+    fileInfo.source = data.content || '';
+    res.end(JSON.stringify({
+      status: 0,
+      message: '',
+      data: {}
+    }));
+    return;
+  }
+
   fs.open(path.resolve(data.file), 'w', (err, fd) => {
     res.writeHead(200, {
       'Content-Type': 'application/json'
