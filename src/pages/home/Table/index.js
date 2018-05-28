bdec8cef906c8e59a305843d7ee3a71cf15d3568
zhixuan.tan <zhixuan.tan@qunar.com>
2018-05-28 15:52:49 +0800

    feat: 新增配置文件一键开启、关闭

--- src/pages/home/Table/index.js
+++ src/pages/home/Table/index.js
@@ -11,12 +11,20 @@ import './styles.less';
 export default class extends React.Component {
   constructor (props, state) {
     super(props, state);
-
+    let files = this.props.files;
     this.state = {
-      files: this.props.files
+      files: files,
+      isAllSelected: this.getDefaultStatus(files)
     };
-
+    
+    this.btns = [];
+    this.btnsLength = 0;
     this.onModalClose = this.onModalClose.bind(this);
+    this.saveFile = this.saveFile.bind(this);
+    this.switchAllStatus = this.switchAllStatus.bind(this);
+    this.switchStatus = this.switchStatus.bind(this);
+    this.getRefs = this.getRefs.bind(this);
+    this.checkAllSwitch = this.checkAllSwitch.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -36,7 +44,13 @@ export default class extends React.Component {
           <thead>
             <tr>
               <th>File Path</th>
-              <th>State</th>
+              <th>
+                State
+                <label className="form-switch form-switch-all">
+                  <input type="checkbox" checked={this.state.isAllSelected} onClick={this.switchAllStatus}/>
+                  <i className="form-icon"></i>
+                </label>
+              </th>
               <th>Domain Count</th>
               <th>Config Type</th>
               <th>Operate</th>
@@ -51,11 +65,11 @@ export default class extends React.Component {
                 const isEnable = enable ? <span className="enable">enabled</span>:<span className='disable'>disabled</span>;
 
                 return (
-                  <tr className="" key={file} key={file}>
+                  <tr className="" key={file}>
                     <td className="color-blue">{file}</td>
                     <td className="status-switch">
                       <label className="form-switch">
-                        <input type="checkbox" onClick={this.switchStatus.bind(this, file, enable, port, fileType)} checked={isChecked}/>
+                        <input type="checkbox" onClick={this.switchStatus.bind(this, file, enable, port, fileType)} checked={isChecked} ref={this.getRefs}/>
                         <i className="form-icon"></i>
                       </label>
                     </td>
@@ -76,17 +90,66 @@ export default class extends React.Component {
     )
   }
 
+  getDefaultStatus(obj) {
+    return Object.entries(obj).every((item)=>{
+      return item[1].enable === true;
+    })
+  }
+
+  getRefs(ele) {
+    this.btns[this.btnsLength++] = ele;
+  }
+
+  switchAllStatus() {
+    let state = this.state;
+    this.btns.forEach((ele) => {
+    if (!state.isAllSelected) { // 点击前为未选中状态
+      if (!ele.checked) {
+        ele.click();
+      }
+    } else {
+      if (ele.checked) {
+        ele.click();
+      }
+    }
+      
+    });
+    this.setState({
+      isAllSelected: !this.state.isAllSelected
+    })
+  }
+
+  checkAllSwitch(arr) {
+    // 判断当前files对象，是否是全部都为enable或者存在一个不是enable
+    // 因此，该函数只应该在enable配置文件时调用以检测
+    let bool = arr.filter((item) => {
+      return item[1].enable === true;
+    }).length - arr.length >= -1;
+    return this.state.isAllSelected || bool;
+  }
+
+
   switchStatus(file, enable, port, type) {
+    
     const files = this.state.files;
     const actionType = !enable ? 'enableFile':'disableFile';
     const me = this;
     const fileType = {fileType: type, filePath: file};
-
+    let isAllSelected = true;
+    if ('disableFile' === actionType) {
+      isAllSelected = false;
+    } else {
+      let bool = !this.checkAllSwitch(Object.entries(files));
+      if (bool) {
+        isAllSelected = false;
+      }
+    }
     files[file].enable = !enable;
 
     fetch('http://127.0.0.1:' + port + '/api?action='+actionType +'&params='+JSON.stringify(fileType)).then(function (res) {
       me.setState({
-        files
+        files,
+        isAllSelected
       });
     }).catch(function (err) {
       console.log(err);
@@ -94,6 +157,7 @@ export default class extends React.Component {
 
     return false;
   }
+
 
 
   renderDialog() {
